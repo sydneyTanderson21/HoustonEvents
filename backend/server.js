ac472c5031a10705c6b358ee63dc942963c162f9
Sydney Anderson <andersonsydney58@gmail.com>
2023-03-15 16:26:17 -0500

    Adding files

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,30 @@
 const express = require('express'); 
-const mongodb = require('mongodb');
 const cors = require('cors');
 const path = require('path');
 const router = express.Router()
 
+const { Pool, Client } = require('pg');
+
+//set server port
+// CONSTANTS
+const port = process.env.PORT || 5000; 
 
 const app = express(); 
 
 // Enable CORS security headers
 app.use(cors())
 
-//add middlewares
-// Have Node serve the files for our built React app
+// telling ExpressJS to look up static files in the react app build folder.
 const production = process.env.NODE_ENV === 'production'
 if (production) {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 } else {
     app.use(express.static(path.join(__dirname, '../frontend')))
 }
-
-// const root = path.join(__dirname, '../frontend');
-// app.use(express.static(root));
-
 // // to parse POST method
 // app.use(express.json())
 // app.use(express.urlencoded({ extended: true }));
 
-//set server port
-const port = process.env.PORT || 5000; // or 3001
 
 // create a GET route
 app.get('/api', (req, res) => { 
@@ -38,24 +35,29 @@ app.get('/api', (req, res) => {
 
 // server index.html for unknown paths
 // All other GET requests not handled before will return our React app
+// may need to move index.html out of public folder since thats default react code!!
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../frontend/public', 'index.html'));
 });
+// app.use('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
+
+const client = new Client({
+  user: 'user',
+  host: 'postgres',
+  database: 'db',
+  password: 'pass',
+  port: 5432,
+});
+client.connect()
+client.query('SELECT NOW()', (err, res) => {
+  console.log("Error or response:: ", err, res)
+  client.end()
+});
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 });
-
-//creating a simple database in MongoDB
-//create a mongoDB Client object, then specify a connecting url
-
-// var MongoClient = require('mongodb').MongoClient;
-// var url = "mongodb://localhost:27017/mydb";
-
-// MongoClient.connect(url, function(err, db) {
-//   if (err) throw err;
-//   console.log("Database created!");
-//   db.close();
-// });
